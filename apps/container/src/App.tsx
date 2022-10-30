443ddabc8ab89792d2f21d81b3296dedc559585b
lschulzes <lucassilvaiee@gmail.com>
2022-10-30 17:25:13 -0300

    use yarn

--- apps/container/src/App.tsx
+++ apps/container/src/App.tsx
@@ -3,6 +3,10 @@ import MarketingApp from "./components/MarketingApp";
 export const App = () => {
   return (
     <>
+      <h1>Hello World</h1>
+
+      <hr />
+
       <MarketingApp />
     </>
   );
